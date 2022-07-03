e75b271cc4c2e817a1a289b73e28945590bc537c
chino <akahemenokwu@gmail.com>
2022-07-03 09:52:34 -0700

    late push!

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always returns 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'q' || a == 'e')
+		{
+			continue;
+		}
+		putchar(a);
+	}
+		return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always returns 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+		putchar(a);
+	puchar('\n');
+	return (0);
+}
