522441b6b693fd18b9a162cc916a51275c621099
chino <akahemenokwu@gmail.com>
2022-07-14 06:34:46 -0700

    mandaory

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,12 +14,11 @@ char *_strcat(char *dest, char *src)
 	i = 0;
 	j = 0;
 
-	while (dest[i] != '\0')
+	while (dest[i])
 		i++;
-	while (src[j] != '\0')
+	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
-		j++;
 		i++;
 	}
 	dest[i] = '\0';
