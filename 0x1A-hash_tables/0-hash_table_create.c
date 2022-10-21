3cb57d258e017f5192f348acd8fb2e93aa8c1ecd
Chino Aka <akahemenokwu@gmail.com>
2022-10-21 06:49:37 -0700

    uwuu

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a hash table
+ * @size: Hash table size
+ * Return: Pointer to the new ht
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table = NULL;
+	hash_node_t **array = NULL;
+	unsigned long int index;
+
+	/* allocate table */
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+
+	hash_table->size = size;
+
+	/* allocate array */
+	array = malloc(size * sizeof(hash_node_t *));
+	if (array == NULL)
+		return (NULL);
+
+	/* initialize array */
+	for (index = 0; index < size; index++)
+		array[index] = NULL;
+
+	hash_table->array = array;
+	return (hash_table);
+}
